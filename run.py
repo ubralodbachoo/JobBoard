34b3abfee1a98b0dc9137d874c2da390b320bbd4
Ubralodbachoo <bachomirzashvili2007@gmail.com>
2026-06-22 01:25:26 +0400

    refactor: Simplify app initialization in run.py
    - Replaced direct imports of db and models with create_app function for better encapsulation.
    - Removed shell context processor to streamline the application setup.

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
-from app import db
-from app.models import User, Job
+from app import create_app
 from wsgi import app
 
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Job': Job}
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
